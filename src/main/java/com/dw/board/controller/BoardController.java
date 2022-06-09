8f33c425380561128036c2dce920d08de938757f
YangDaEun <das03041@gmail.com>
2022-06-09 10:30:40 +0900

    fix : 주석 설명 수정

--- src/main/java/com/dw/board/controller/BoardController.java
+++ src/main/java/com/dw/board/controller/BoardController.java
@@ -8,7 +8,7 @@ public class BoardController {
 	@GetMapping("/home")
 	public String callHomepage() {
 		//템플릿 엔진을 사용하게 되면 리턴타입은 String이어야 한다.
-		//why? jsp파일 script에 js파일 주소를 설정 해주었기 때문에 jsp 파일명을 리턴해준다고 생각하면 된다.
+		//why? jsp 파일명을 리턴해준다고 생각하면 된다.
 		return "index"; // jsp파일 이름으로 return 해주기
 	}
 }
